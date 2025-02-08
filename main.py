8d707e3a5c46be8dd46e8e79b9b9b3625844b2fc
araymaratova <araymrtv@gmail.com>
2025-02-09 02:39:27 +0500

    Update main.py

--- main.py
+++ main.py
@@ -48,3 +48,20 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+
+from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Бот работает!"
+
+def run():
+    app.run(host="0.0.0.0", port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
